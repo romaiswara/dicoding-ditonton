efcd30ded1755ad03198dac0e319e67623aaf1e0
Roma Iswara <roma.iswara@sinarmasmining.com>
2023-06-28 06:12:55 +0700

    [add] about package

--- about/lib/about_page.dart
+++ about/lib/about_page.dart
@@ -1,8 +1,8 @@
-import 'package:ditonton/common/constants.dart';
+import 'package:core/core.dart';
 import 'package:flutter/material.dart';
 
 class AboutPage extends StatelessWidget {
-  static const ROUTE_NAME = '/about';
+  const AboutPage({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -26,7 +26,7 @@ class AboutPage extends StatelessWidget {
                 child: Container(
                   padding: const EdgeInsets.all(32.0),
                   color: kMikadoYellow,
-                  child: Text(
+                  child: const Text(
                     'Ditonton merupakan sebuah aplikasi katalog film yang dikembangkan oleh Dicoding Indonesia sebagai contoh proyek aplikasi untuk kelas Menjadi Flutter Developer Expert.',
                     style: TextStyle(color: Colors.black87, fontSize: 16),
                     textAlign: TextAlign.justify,
@@ -38,7 +38,7 @@ class AboutPage extends StatelessWidget {
           SafeArea(
             child: IconButton(
               onPressed: () => Navigator.pop(context),
-              icon: Icon(Icons.arrow_back),
+              icon: const Icon(Icons.arrow_back),
             ),
           )
         ],
