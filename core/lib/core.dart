1105d81e90ed5c358691c6ad84f3aecc3768554a
Roma Iswara <roma.iswara@sinarmasmining.com>
2023-06-28 06:58:15 +0700

    [fix] barrel

--- core/lib/core.dart
+++ core/lib/core.dart
@@ -1,10 +1,5 @@
 library core;
 
-export 'styles/colors.dart';
-export 'styles/text_styles.dart';
-export 'utils/constants.dart';
-export 'utils/exception.dart';
-export 'utils/failure.dart';
-export 'utils/routes.dart';
-export 'utils/state_enum.dart';
+export 'db/db.dart';
+export 'styles/styles.dart';
 export 'utils/utils.dart';
--- core/lib/utils/utils.dart
+++ core/lib/utils/utils.dart
@@ -1,3 +1,9 @@
 import 'package:flutter/widgets.dart';
 
+export 'constants.dart';
+export 'exception.dart';
+export 'failure.dart';
+export 'routes.dart';
+export 'state_enum.dart';
+
 final RouteObserver<ModalRoute> routeObserver = RouteObserver<ModalRoute>();
