0a77c7cee553b91bacc10c9c1488fe7384b75ec1
Roma Iswara <roma.iswara@sinarmasmining.com>
2023-06-29 13:09:38 +0700

    [sanitize-code] remove unused code

--- tv_series/lib/presentation/provider/provider.dart
+++
@@ -1,8 +0,0 @@
-export 'airing_today_tv_series_notifier.dart';
-export 'on_the_air_tv_series_notifier.dart';
-export 'popular_tv_series_notifier.dart';
-export 'top_rated_tv_series_notifier.dart';
-export 'tv_series_detail_notifier.dart';
-export 'tv_series_list_notifier.dart';
-export 'tv_series_search_notifier.dart';
-export 'watchlist_tv_series_notifier.dart';
